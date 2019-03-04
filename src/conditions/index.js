27e0e8d915553f6a2df0aa6895afb2e58f04d8b3
Robert Crous <robert.crous21@gmail.com>
2019-03-04 13:18:08 +0200

    Sensible export/import systems for package use

---
+++ src/conditions/index.js
@@ -0,0 +1,9 @@
+import andCondition from './andCondition';
+import orCondition from './orCondition';
+import notCondition from './notCondition';
+
+export default {
+  andCondition,
+  orCondition,
+  notCondition,
+};
